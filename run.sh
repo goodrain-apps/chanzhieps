556ead69a2a79e56870f18ee8f27051b42ff4de7
zhouyq <zyq916@gmail.com>
2016-04-14 18:56:43 +0800

    持久化目录逻辑修改

--- run.sh
+++ run.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-Dirs="www/data config tmp"
+Dirs="system/config system/module system/tmp www/data www/template"
 PermanentDir="/data"
 AppDir="/app"
-UserCfg="${PermanentDir}/config/my.php"
-InstallFile="${AppDir}/www/sys/install.php"
-UpgradeFile="${AppDir}/www/sys/upgrade.php"
+UserCfg="${PermanentDir}/system/config/my.php"
+InstallFile="${AppDir}/www/install.php"
+UpgradeFile="${AppDir}/www/upgrade.php"
 
 
 # 在持久化存储中创建需要的目录
 for d in $Dirs
 do
+  # 检测是否包含子目录
+  subdir=$(dirname $d)
+
   if [ ! -d ${PermanentDir}/${d} ] ;then
   
-    if [ "$d" == "www/data" ];then
-      [ -d ${AppDir}/${d} ] && mkdir -pv ${PermanentDir}/www && mv ${AppDir}/${d} ${PermanentDir}/www
+    if [ "$subdir" == "." ];then
+      [ -d ${AppDir}/${d} ] && mv ${AppDir}/${d} ${PermanentDir}/${d} || mkdir -pv ${PermanentDir}/${d}
+    else
+      [ ! -d ${PermanentDir}/$subdir ] && mkdir -pv ${PermanentDir}/$subdir
+      [ -d ${AppDir}/${d} ] && mv ${AppDir}/${d} ${PermanentDir}/$subdir
     fi
   
-    [ -d ${AppDir}/${d} ] && mv ${AppDir}/${d} ${PermanentDir}/${d} || mkdir -pv ${PermanentDir}/${d}
   else
     mv ${AppDir}/${d} ${AppDir}/${d}.bak
   fi
-
   ln -s ${PermanentDir}/${d} ${AppDir}/${d}
 done
 
